f85fe4c7c1fd0707342d043499b944fc685b2ead
ListerioAxxe <langua095@gmail.com>
2021-06-06 14:54:06 +0300

    Универсальное хранилище

---
+++ src/main/java/ru/job4j/generics/store/Role.java
@@ -0,0 +1,13 @@
+package ru.job4j.generics.store;
+
+public class Role extends Base {
+
+    public Role(String id) {
+        super(id);
+    }
+
+    @Override
+    public String getId() {
+        return super.getId();
+    }
+}
