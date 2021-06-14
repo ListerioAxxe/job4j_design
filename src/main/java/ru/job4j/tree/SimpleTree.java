e69973681ad51cbd91169d6442f709c86ee5fa11
ListerioAxxe <langua095@gmail.com>
2021-06-14 13:28:51 +0300

    SimpleTree

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,37 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> rslt = findBy(parent);
+        if (findBy(child).isPresent() || rslt.isEmpty()) {
+            return false;
+        }
+        rslt.get().getChildren().add(new Node<>(child));
+        return true;
+     }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.getValue().equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.getChildren());
+        }
+        return rsl;
+    }
+}
