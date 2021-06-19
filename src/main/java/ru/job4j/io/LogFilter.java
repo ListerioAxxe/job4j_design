74cc8952ebbec9d660b320c6f25b57963b3484c6
ListerioAxxe <langua095@gmail.com>
2021-06-19 16:26:54 +0300

    IO3

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> rslList = new ArrayList<>();
+        try (var in = new BufferedReader(new FileReader(file))) {
+           rslList = in.lines()
+                   .filter(x -> x.contains("404"))
+                   .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rslList;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
