4f510d71ab03367b0152d7ddc5bba453b2591d8e
ListerioAxxe <langua095@gmail.com>
2021-07-07 12:38:44 +0300

    join

--- src/main/resources/task.sql
+++ src/main/resources/task.sql
@@ -11,11 +11,11 @@ select * from departments d left join employes e on e.id_department = d.id where
 
 select *
 from employes e
-left join departments d on d.id = e.id_department;
+right join departments d on d.id = e.id_department;
 
 select *
-from employes e
-right join departments d on d.id = e.id_department;
+from departments d
+left join employes e on d.id = e.id_department;
 
 select t1.name Boys, t2.name Girls from teens t1 cross join teens t2 where  t1.gender != t2.gender and t1.gender like 'm%';
 
