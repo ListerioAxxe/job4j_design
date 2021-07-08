5fb64c7383733758032318a827dd9eb9bef04f80
ListerioAxxe <langua095@gmail.com>
2021-07-08 13:14:06 +0300

    jdbc

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,58 +1,24 @@
 package ru.job4j.jdbc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.net.URL;
+
+import ru.job4j.io.Config;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        URL path = ClassLoader.getSystemResource("ap.properties");
-        var findArg = new FindArg(path.getPath());
-        findArg.find();
+        Config config = new Config("ap.properties");
+        config.load();
         try (Connection connection = DriverManager
-                .getConnection(findArg.get("url"), findArg.get("login"), findArg.get("password"))) {
+                .getConnection(config.value("url"),
+                 config.value("login"), config.value("password"))) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
             System.out.println(metaData.getURL());
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-private static class FindArg {
-        private static final Logger LOG = LoggerFactory.getLogger(FindArg.class.getName());
-        private final String path;
-        private final Map<String, String> valuesOf;
-
-        public FindArg(String path) {
-            this.path = path;
-            valuesOf = new HashMap<>();
-        }
-
-        private void find() {
-            try (var reader = new BufferedReader(new FileReader(path))) {
-                reader.lines()
-                        .forEach(x -> {
-                            String[] s = x.split("=");
-                            if (s.length == 2) {
-                                valuesOf.put(s[0], s[1]);
-                            }
-                        });
-            } catch (Exception e) {
-                LOG.error("Invalid file", e);
-            }
-        }
-
-        public String get(String key) {
-           return valuesOf.get(key);
         }
     }
 }
