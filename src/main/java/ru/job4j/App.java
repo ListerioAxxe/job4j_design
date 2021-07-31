06cab75ce9b621156621fbadd6eb499b39536d0f
ListerioAxxe <langua095@gmail.com>
2021-07-31 18:46:21 +0300

    corr

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,11 +1,14 @@
 package ru.job4j;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[][] arr = new int[][] {{1, 2}, {3, 4}};
     }
 }
--- src/main/java/ru/job4j/garbage/cache/DirFileCache.java
+++ src/main/java/ru/job4j/garbage/cache/DirFileCache.java
@@ -1,25 +1,25 @@
-package ru.job4j.garbage.cache;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-public class DirFileCache extends AbstractCache<String, String> {
-
-    private final String cachingDir;
-
-    public DirFileCache(String cachingDir) {
-        this.cachingDir = cachingDir;
-    }
-
-    @Override
-    protected String load(String key) {
-        String result = null;
-        try {
-            result = Files.readString(Path.of(cachingDir, key));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-}
+//package ru.job4j.garbage.cache;
+//
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//
+//public class DirFileCache extends AbstractCache<String, String> {
+//
+//    private final String cachingDir;
+//
+//    public DirFileCache(String cachingDir) {
+//        this.cachingDir = cachingDir;
+//    }
+//
+//    @Override
+//    protected String load(String key) {
+//        String result = null;
+//        try {
+//            result = Files.readString(Path.of(cachingDir, key));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return result;
+//    }
+//}
